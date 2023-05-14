c8dbb3e309b758ce810432d962e07c32f26d5415
kud884 <133370993+kud884@users.noreply.github.com>
2023-05-14 12:27:00 +0900

    Add files via upload

---
+++ jastaijiriJS.js
@@ -0,0 +1,55 @@
+const img = document.querySelector('img');
+const audios = [
+  new Audio('audio1.mp3'),
+  new Audio('audio2.mp3'),
+  new Audio('audio3.mp3'),
+  new Audio('audio4.mp3'),
+  new Audio('audio5.mp3'),
+  new Audio('audio6.mp3'),
+  new Audio('audio7.mp3'),
+  new Audio('audio8.mp3'),
+  new Audio('audio9.mp3'),
+  new Audio('audio10.mp3'),
+  new Audio('audio11.mp3')
+];
+
+img.addEventListener('click', () => {
+  // ランダムに音声ファイルを再生する
+  const index = Math.floor(Math.random() * audios.length);
+  audios[index].play();
+  
+  // 別の画像に切り替える
+  img.src = 'image2.png';
+  
+  // 0.5秒後に元の画像に戻す
+  setTimeout(() => {
+    img.src = 'image.png';
+  }, 2000); // 2000ミリ秒 = 2秒
+});
+
+// インデックスを保持する変数
+let currentIndex = 0;
+
+// タッチイベントの処理
+img.addEventListener('touchstart', () => {
+  // 現在のインデックスに対応する音声を再生する
+  audios[currentIndex].play();
+  
+  // インデックスを次に進める
+  currentIndex++;
+  
+  // インデックスが配列の長さを超えた場合、最初に戻す
+  if (currentIndex >= audios.length) {
+    currentIndex = 0;
+  }
+  
+  // 別の画像に切り替える
+  img.src = 'image2.png';
+  
+  // 0.5秒後に元の画像に戻す
+  setTimeout(() => {
+    img.src = 'image.png';
+  }, 2000);
+});
+
+
